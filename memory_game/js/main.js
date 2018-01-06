e81619b3f163e749b716a4a2336bf3f15dfe2ccf
Jonathan Hicks <JonathanKHicks@Macintosh-60fb42ee05ec.local>
2018-01-06 15:42:54 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push("cardOne");
+cardsInPlay.push("cardTwo");
+
+console.log("User flipped queen");
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+	alert("you have found a match!")
+}
+else {
+	alert("Sorry, try again.")
+}
